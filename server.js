744102e6a1e684d9af304c5e0287920f5822a2bd
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-03-22 15:58:06 -0700

    parsed object from the website - next step creatre sequlizer for that

--- server.js
+++ server.js
@@ -15,9 +15,190 @@ app.set("view engine", "handlebars");
 
 app.use(require("./routes"));
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("Listening on PORT", PORT);
 });
+
+
+
+
+
+
+
+axios.get("http://redmondlocal.com").then((res) => {
+  const categories = [];
+  const $ = cheerio.load(res.data);
+
+  $("h4.has-text-align-center").each(function (i, element) {
+
+    var obj = {
+      title: $(this).text(),
+      entries: []
+    }
+
+    // db.Headers.create({ title: title })
+    //   .then(function (header) {
+
+    var cols = $(this).siblings('.wp-block-columns').children()
+    $(cols).each(function (i, element) {
+      var entry = {
+        //headerId: header.id,
+        title: $(this).find('strong').text(),
+        title_link: $(this).find('strong').find('a').attr("href") || '',
+        option: $(this).find('em').text() || '',
+        option_link: $(this).find('em').find('a').attr("href") || '',
+        text: $(this).text()
+      }
+      // db.Entry.create(entry)
+
+      if (entry.title) {
+        obj.entries.push(entry)
+      }
+    })
+    console.log(obj)
+    categories.push(obj)
+
+    // })
+
+  })
+
+  // $(".entry-content").children(".wp-block-group.alignfull").each((index,element)=>{
+
+  //   $(element).find("h4","strong","a").each((i,helem)=>{
+  //     categories[index]={
+  //       title:$(this).find("h4"),
+  //       name:$(this).find("strong"),
+  //       link:$(this).find("a").attr("href")
+  //     };
+
+  //     console.log(categories);
+
+  //   })
+  // var results = $(".entry-content").children(".wp-block-group.alignfull").children();
+  // results.each((i,res)=>{
+  //   var title = $(res).children("h4").text();
+  //   var name = res.children[1];
+  //   console.log(title);
+
+  // })
+  //   $(".entry-content").children(".wp-block-group.alignfull").not(':first-child').children().each((index, element) => {
+
+  //     const title = $(element).children("h4").text();
+  //     const names = $(element).children(".wp-block-columns.alignwide").children(".wp-block-column").children("p").children("strong").text();
+  //     const link = $(element).children(".wp-block-columns.alignwide").children(".wp-block-column").children("p").children("a").attr("href");
+  //     categories[index] = { title, names, link };
+  //   })
+  //   console.log(categories);
+
+});
+
+
+// $(".wp-block-group .alignfull").each((index,element)=>{
+    //   const title = $(element).children("h4");
+    // })
+
+    //categories
+    // $("h4.has-text-align-center").each((index, element)=>{
+    //   const title = $(element).text();
+    //   categories[index]={title:title};
+
+    // });
+    // console.log(categories);
+
+    // $(".has-text-align-center").each((index, element)=>{
+    //   const title = $(element).children('h4').text();
+    //   categories[index] = {title:title};
+
+    // });
+    // console.log(categories);
+
+
+    // console.log($('.has-text-align-center').children('strong').first().text());
+    // console.log($('.entry-content').find('h4').text());
+
+    // creating object for categories
+    // $('.entry-content').find('h4').each((index,element)=>{
+    //   const title=$(element).text();
+    //   categories[index]={title};
+    // })
+    // console.log(categories);
+
+  //  console.log($('.entry-content').find('strong').text());
+
+  // $(".entry-content").find('strong').each((index,element)=>{
+  //   const name = $(element).text();
+  //   categories[index]={name};
+  // })
+  // console.log(categories);
+
+
+
+  // $(".wp-block-group.alignfull").find('strong').each((index,element)=>{
+  //   const name = $(element).text();
+  //   categories[index]={name};
+  // })
+  // console.log(categories);
+
+
+  // console.log($("#main").children(".wp-block-group.alignfull").html());
+  // $(".wp-block-group.alignfull").find('strong').each((index,element)=>{
+  //   const name = $(element).text();
+  //   categories[index]={name};
+  // })
+  // console.log(categories);
+
+  // console.log($(".wp-block-columns.alignwide").parent().html());
+
+
+
+
+//barely working but object
+  // $(".entry-content").children(".wp-block-group.alignfull").not(':first-child').children(".wp-block-group__inner-container").each((index,element)=>{
+
+  //   const title = $(element).children("h4").text();
+  //   const names = $(element).children(".wp-block-columns.alignwide").children(".wp-block-column").children("p").children("strong").text();
+  //   const link = $(element).children(".wp-block-columns.alignwide").children(".wp-block-column").children("p").children("a").attr("href");
+  //   categories[index]={title,names,link};
+  // })
+  // console.log(categories);
+
+
+
+
+
+
+        // .then(groupName=>{
+      //   $(element).find("strong").each((s,selem)=>{
+      //     const names = $(selem).text();
+      //   })
+      //   .then(names=>{
+      //     $(element).find("a").attr("href").each((l,lelem)=>{
+      //       const links = $(lelem).text();
+      //     })
+      //     .then(links=>{
+      //       categories.push({
+      //         title:groupName,
+      //         names:names,
+      //         link:links
+      //       })
+      //     })
+      //   })
+      // })
+  //   const link = $(element).children(".wp-block-columns.alignwide").children(".wp-block-column").children("p").children("a").attr("href");
+  //   const names = $(element).find("strong").each((i, el)=>{
+  //     categories.push({
+  //       title:title,
+  //       names:$(el).text(),
+  //       link:link
+  //     })
+  //   })
+
+
+  // })
+  // console.log(categories);
+
+
+
 
 
 
@@ -58,79 +239,3 @@ app.listen(PORT, function() {
 
 // };
 // getData();
-
-
-
-
-axios.get("http://redmondlocal.com").then((res)=>{
-    const categories = [];
-    const $ = cheerio.load(res.data);
-
-    // $(".wp-block-group .alignfull").each((index,element)=>{
-    //   const title = $(element).children("h4");
-    // })
-
-    //categories
-    // $("h4.has-text-align-center").each((index, element)=>{
-    //   const title = $(element).text();
-    //   categories[index]={title:title};
-
-    // });
-    // console.log(categories);
-
-    // $(".has-text-align-center").each((index, element)=>{
-    //   const title = $(element).children('h4').text();
-    //   categories[index] = {title:title};
-     
-    // });
-    // console.log(categories);
-
-
-    // console.log($('.has-text-align-center').children('strong').first().text());
-    // console.log($('.entry-content').find('h4').text());
-
-    // creating object for categories
-    // $('.entry-content').find('h4').each((index,element)=>{
-    //   const title=$(element).text();
-    //   categories[index]={title};
-    // })
-    // console.log(categories);
-
-  //  console.log($('.entry-content').find('strong').text());
-  
-  // $(".entry-content").find('strong').each((index,element)=>{
-  //   const name = $(element).text();
-  //   categories[index]={name};
-  // })
-  // console.log(categories);
-
-
- 
-  // $(".wp-block-group.alignfull").find('strong').each((index,element)=>{
-  //   const name = $(element).text();
-  //   categories[index]={name};
-  // })
-  // console.log(categories);
-
-
-  // console.log($("#main").children(".wp-block-group.alignfull").html());
-  // $(".wp-block-group.alignfull").find('strong').each((index,element)=>{
-  //   const name = $(element).text();
-  //   categories[index]={name};
-  // })
-  // console.log(categories);
- 
-  // console.log($(".wp-block-columns.alignwide").parent().html());
-  $(".entry-content").children(".wp-block-group.alignfull").each((index,element)=>{
-    const title = $(element).find("h4").text();
-    const names = $(element).find("strong").text();
-    categories[index]={title,names};
-  })
-  console.log(categories);
-});
-
-// console.log($(".entry-content").children(".wp-block-group.alignfull").children(".wp-block-group__inner-container").children("h4").html());
-
-
-
-
