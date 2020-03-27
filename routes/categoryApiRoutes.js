779ca1a0043cde1341f156494075e58a1ae7780f
Gil Solis <gilsolisuw@gmail.com>
2020-03-27 11:25:18 -0700

    styling

--- routes/categoryApiRoutes.js
+++ routes/categoryApiRoutes.js
@@ -13,42 +13,41 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 
-  app.get("/api/categories", function(req, res) {
-db.Category.findAll({
-  include:[db.Place]
-})
-.then(function(dbCategory){
-  res.json(dbCategory);
-})
-  });
-
-
-  app.get("/api/categories/:id", function (req, res) {
-    db.Category.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.Place]
-    }).then(function (dbCategory) {
-      res.json(dbCategory);
-    })
-  });
-
-  app.post("/api/categories", function(req, res) {
-    db.Category.create(req.body).then(function(dbCategory) {
-      res.json(dbCategory);
+    app.get("/api/categories", function(req, res) {
+        db.Category.findAll({
+                include: [db.Place]
+            })
+            .then(function(dbCategory) {
+                res.json(dbCategory);
+            })
     });
-  });
 
-  app.delete("/api/categories/:id", function(req, res) {
-    db.Category.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbCategory) {
-      res.json(dbCategory);
+
+    app.get("/api/categories/:id", function(req, res) {
+        db.Category.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.Place]
+        }).then(function(dbCategory) {
+            res.json(dbCategory);
+        })
     });
-  });
+
+    app.post("/api/categories", function(req, res) {
+        db.Category.create(req.body).then(function(dbCategory) {
+            res.json(dbCategory);
+        });
+    });
+
+    app.delete("/api/categories/:id", function(req, res) {
+        db.Category.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbCategory) {
+            res.json(dbCategory);
+        });
+    });
 
 };
-
--- server.js
+++ server.js
@@ -15,43 +15,42 @@ const hbs = require("express-handlebars");
 app.engine("handlebars", hbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-require("./routes/categoryApiRoutes.js")(app);
+// require("./routes/categoryApiRoutes.js")(app);
 app.use(require("./routes"));
 
-db.sequelize.sync({}).then(function () {
-  axios.get("http://redmondlocal.com").then(({ data }) => {
-    const $ = cheerio.load(data);
+db.sequelize.sync({}).then(function() {
+    axios.get("http://redmondlocal.com").then(({ data }) => {
+        const $ = cheerio.load(data);
 
-    $("h4.has-text-align-center").each(function (i, element) {
-      // console.log($(this));
-      db.Category.create({ name: $(element).text() })
-        .then(function (category) {
-          // console.log(category.dataValues.id)
-          // db.Place.create({ name: "Peter" })
-          // console.log("===========>", $(this).find('.wp-block-columns').children());
-          var cols = $(element).siblings('.wp-block-columns').children();
-          //console.log(cols);
+        $("h4.has-text-align-center").each(function(i, element) {
+            // console.log($(this));
+            db.Category.create({ name: $(element).text() })
+                .then(function(category) {
+                    // console.log(category.dataValues.id)
+                    // db.Place.create({ name: "Peter" })
+                    // console.log("===========>", $(this).find('.wp-block-columns').children());
+                    var cols = $(element).siblings('.wp-block-columns').children();
+                    //console.log(cols);
 
-          // console.log(cols);
+                    // console.log(cols);
 
-          cols.each(function (i, element) {
-            db.Place.create({
-              categoryId: category.dataValues.id,
-              name: $(this).find('strong').text(),
-              titleLink: $(this).find('a').attr("href") || '',
-              optionText: $(this).find('em').text() || '',
-              optionLink: $(this).find('em').find('a').attr("href") || '',
-              text: $(this).text().replace(/([A-Z])/g, ' $1').trim().replace(/\n/g, "")
-            })
-          })
-        })
-      // .catch(function(err){console.log (err)})
+                    cols.each(function(i, element) {
+                        db.Place.create({
+                            categoryId: category.dataValues.id,
+                            name: $(this).find('strong').text(),
+                            titleLink: $(this).find('a').attr("href") || '',
+                            optionText: $(this).find('em').text() || '',
+                            optionLink: $(this).find('em').find('a').attr("href") || '',
+                            text: $(this).text().replace(/([A-Z])/g, ' $1').trim().replace(/\n/g, "")
+                        })
+                    })
+                })
+                // .catch(function(err){console.log (err)})
+        });
+
+    })
+
+    app.listen(PORT, function() {
+        console.log("Listening on PORT", PORT);
     });
-
-  })
-
-  app.listen(PORT, function () {
-    console.log("Listening on PORT", PORT);
-  });
 });
-
